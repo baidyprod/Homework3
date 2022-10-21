c8bf0d94aed740fdd5c4e07490ca1c535289cc82
baidyprod <dima220920041@gmail.com>
2022-10-21 19:27:48 +0300

    V.3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ count=len(values)
 
 if count == 1:
     a = int(input_str)
-    if a == 0:
+    if a <= 0:
         print('There can\'t be a circle with 0 radius')
     else:
         perimeter = 2*a*3.14
@@ -17,7 +17,7 @@ elif count == 2:
     a1, b1 = values
     a = int(a1)
     b = int(b1)
-    if a == 0 or b == 0:
+    if a <= 0 or b <= 0:
         print('There can\'t be a square or a rectangle with 0 side')
     else:
         if a == b:
@@ -33,7 +33,7 @@ elif count == 3:
     a = int(a1)
     b = int(b1)
     c = int(c1)
-    if a == 0 or b == 0 or c == 0:
+    if a <= 0 or b <= 0 or c <= 0:
         print('There can\'t be a triangle with 0 side')
     elif a + b <= c or a + c <= b or b + c <= a:
         print('There can\'t be a triangle where sum of 2 sides is less or equal to the 3-rd one')
